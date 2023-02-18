34570cb74263bafa67020064f67eb5d4b3e64913
Const <56368289+tasknetdeveloper@users.noreply.github.com>
2023-02-18 21:03:47 +0600

    first

---
+++ Abstract/RedisAbstract.cs
@@ -0,0 +1,71 @@
+﻿using Redis.OM;
+using LoggerSpace;
+using Redis.OM.Searching;
+using System.Linq.Expressions;
+using StackExchange.Redis;
+
+namespace RedisWorkOMSpace
+{
+    public sealed class RedisAbstract<T> : RedisAbstractBase<T> where T : class, new()
+    {
+        RedisConnectionProvider? provider = null;
+        public RedisAbstract(Log log, RedisConnectionProvider? provider, string indexName)
+        {
+            this.log = log;
+            Ini(provider, $"{indexName}-idx");
+        }
+
+        public T[]? GetSearchResult(Expression<Func<T, bool>>? expression = null)
+        {
+            T[]? result = null;
+            if (list == null || expression==null) return null;
+
+            try
+            {
+                var r = list.Where(expression);
+                if (r != null)
+                {
+                    result = r.ToArray();
+                }
+                else
+                    log.TraceInfo("GetSearchResult r is null");
+            }
+            catch (RedisException exp0)
+            {
+                log.Error($"GetSearchResult/{exp0.Message}");
+            }
+            catch (Exception exp)
+            {
+                log.Error($"GetSearchResult/{exp.Message}");
+            }
+            return result;
+        }
+
+        public void LoadData(T[] items)
+        {
+            if (items == null) return;
+            if (provider == null) return;
+
+            list = (RedisCollection<T>)provider.RedisCollection<T>();
+            foreach (var item in items)
+            {
+                list.Insert(item);
+            }
+        }
+
+        private void Ini(RedisConnectionProvider? provider_, string indexName)
+        {
+            try
+            {
+                if (provider_ == null) return;
+                provider = provider_;
+
+                CheckIndex(provider_, indexName);
+            }
+            catch (Exception exp)
+            {
+                log.Error($"Ini/{exp.Message}");
+            }
+        }
+    }
+}
---
+++ Abstract/RedisAbstractBase.cs
@@ -0,0 +1,56 @@
+﻿using Redis.OM.Searching;
+using Redis.OM;
+using LoggerSpace;
+
+namespace RedisWorkOMSpace
+{
+    public class RedisAbstractBase<T> where T : class, new()
+    {
+        protected RedisCollection<T>? list = null;
+        protected Log log = new(true);
+
+        public bool Add(T item)
+        {
+            var result = false;
+            if (list == null) return result;
+            try
+            {
+                list.Insert(item);
+                result = true;
+            }
+            catch (Exception exp)
+            {
+                log.Error($"Add/{exp.Message}");
+            }
+            return result;
+        }
+
+        public bool Delete(T item)
+        {
+            var result = false;
+            if (list == null) return result;
+            try
+            {
+                list.Delete(item);
+
+                result = true;
+            }
+            catch (Exception exp)
+            {
+                log.Error($"Delete/{exp.Message}");
+            }
+            return result;
+        }
+
+        protected void CheckIndex(RedisConnectionProvider provider, string indexName)
+        {
+            if (string.IsNullOrEmpty(indexName)) return;
+            indexName= indexName.ToLower();
+            var info = provider.Connection.Execute("FT._LIST").ToArray().Select(x => x.ToString());
+            if (info.All(x => x != indexName))
+            {
+                provider.Connection.CreateIndex(typeof(T));
+            }
+        }
+    }
+}
